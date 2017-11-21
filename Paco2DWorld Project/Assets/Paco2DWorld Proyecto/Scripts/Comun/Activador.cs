dc4cb743d0f6b8b25224e4118186e1569fae2128
Moon Antonio <pinchodev91@gmail.com>
2017-11-21 19:14:24 +0100

    Preset

---
+++ Paco2DWorld Project/Assets/Paco2DWorld Proyecto/Scripts/Comun/Activador.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Activador.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com								        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Activador : MonoBehaviour
+	{
+		#region Metodos
+		/// <summary>
+		/// <para>Cuando se entra a una collision.</para>
+		/// </summary>
+		/// <param name="collision"></param>
+		private void OnCollisionEnter2D(Collision2D collision)// Cuando se entra a una collision
+		{
+			this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+		}
+		#endregion
+	}
+}
