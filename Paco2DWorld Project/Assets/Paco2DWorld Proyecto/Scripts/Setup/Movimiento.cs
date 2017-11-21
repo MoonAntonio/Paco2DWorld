2f733a77c6fcf484f98237e3264f180aee792b9c
Moon Antonio <pinchodev91@gmail.com>
2017-11-21 18:02:50 +0100

    Add Movimiento.cs

---
+++ Paco2DWorld Project/Assets/Paco2DWorld Proyecto/Scripts/Setup/Movimiento.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Movimiento.cs (21/11/2017)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Movimiento del personaje									\\
+// Fecha Mod:		21/11/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Movimiento : MonoBehaviour 
+	{
+
+	}
+}
